c2426a261161ff57e364e0f99e818c9499e3c792
Corentin <corentindubernet1@gmail.com>
2024-10-23 10:42:46 +0200

    exo5

--- exo1.c
+++ exo1.c
@@ -7,7 +7,6 @@ void initGrille(int grille[5][5], int n) {
         }
     }
 }
-
 void affGrille(int grille[5][5], int n) {
     printf_s("\n");
     for (int i = 0; i < n; i++) {
@@ -34,40 +33,56 @@ void affGrille(int grille[5][5], int n) {
     }
     printf_s("\n");
 }
+int Validite(int grille[5][5], int ligne, int colonne, int valeur) {
 
+    for (int j = 0; j < 5; j++) {
+        if (grille[ligne][j] == valeur) {
+            return 0;
+        }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        if (grille[i][colonne] == valeur) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
 void rempgrille(int grille[5][5]) {
     int ligne, colonne, valeur;
 
     while (1) {
-        printf("Entrez le numero de la ligne (0 a 4): ");
+        printf_s("Entrez le numero de la ligne (0 a 4): ");
         scanf_s("%d", &ligne);
 
-        printf("Entrez le numero de la colonne (0 a 4): ");
+        printf_s("Entrez le numero de la colonne (0 a 4): ");
         scanf_s("%d", &colonne);
 
         if (ligne >= 0 && ligne < 5 && colonne >= 0 && colonne < 5) {
-            printf("Donnez votre valeur :");
+            printf_s("Donnez votre valeur : ");
             scanf_s("%d", &valeur);
 
-            grille[ligne][colonne] = valeur;
-
-            printf_s("Grille modifie:\n");
-            affGrille(grille, 5);
+            if (valeur >= 1 && valeur <= 5 && Validite(grille, ligne, colonne, valeur)) {
+                grille[ligne][colonne] = valeur;
+                printf_s("Grille modifiee:\n");
+                affGrille(grille, 5);
+            }
+            else {
+                printf_s("Valeur invalide ou deja presente dans la ligne ou colonne.\n");
+            }
         }
         else {
-            printf_s("ligne ou colonne invalide.\n");
+            printf_s("Ligne ou colonne invalide.\n");
         }
     }
 }
-
 int main() {
     int grille[5][5];
     initGrille(grille, 5);
-    printf("Grille initialisee\n");
+    printf_s("Grille initialisee\n");
     affGrille(grille, 5);
     rempgrille(grille);
     return 0;
 }
-
-
 
