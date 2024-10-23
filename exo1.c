bed375836c5a9b5ec4e9b304533e13c86ff8853c
Corentin <corentindubernet1@gmail.com>
2024-10-23 10:18:51 +0200

    exo3

--- exo1.c
+++ exo1.c
@@ -9,35 +9,53 @@ void initGrille(int grille[5][5], int n) {
 }
 
 void affGrille(int grille[5][5], int n) {
+    printf_s("\n");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            printf("%d ", grille[i][j]);
+            if (grille[i][j] == 0) {
+                printf_s("   ");
+            }
+            else {
+                printf_s("%2d ", grille[i][j]);
+            }
+
+            if (j < n - 1) {
+                printf_s("| ");
+            }
         }
         printf_s("\n");
+
+        if (i < n - 1) {
+            for (int k = 0; k < n; k++) {
+                printf_s("----");
+            }
+            printf_s("\n");
+        }
     }
+    printf_s("\n");
 }
 
 void rempgrille(int grille[5][5]) {
     int ligne, colonne, valeur;
 
     while (1) {
-        printf_s("Entrez le num de la ligne (0 a 4): ");
+        printf("Entrez le numero de la ligne (0 a 4): ");
         scanf_s("%d", &ligne);
 
-        printf_s("Entrez le num de la colonne (0 a 4): ");
+        printf("Entrez le numero de la colonne (0 a 4): ");
         scanf_s("%d", &colonne);
 
         if (ligne >= 0 && ligne < 5 && colonne >= 0 && colonne < 5) {
-            printf_s("donner votre valeur: ");
+            printf("Donnez votre valeur :");
             scanf_s("%d", &valeur);
 
             grille[ligne][colonne] = valeur;
 
-            printf_s("Grille mise a jour:\n");
+            printf_s("Grille modifie:\n");
             affGrille(grille, 5);
         }
         else {
-            printf_s("Erreur: ligne ou colonne invalide.\n");
+            printf_s("ligne ou colonne invalide.\n");
         }
     }
 }
@@ -45,6 +63,7 @@ void rempgrille(int grille[5][5]) {
 int main() {
     int grille[5][5];
     initGrille(grille, 5);
+    printf("Grille initialisee\n");
     affGrille(grille, 5);
     rempgrille(grille);
     return 0;
